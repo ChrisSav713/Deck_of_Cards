644f3dce57d69546b0fa3c0bcb94c5ea52f7320c
ChrisSav713 <106366587+ChrisSav713@users.noreply.github.com>
2022-07-11 17:26:15 -0400

    randomize working

---
+++ classes/deck.py
@@ -0,0 +1,42 @@
+from . import card
+import random
+
+class Deck:
+
+
+    def __init__( self ):
+        suits = [ "spades" , "hearts" , "clubs" , "diamonds" ]
+        self.cards = []
+
+        for s in suits:
+            for i in range(1,14):
+                str_val = ""
+                if i == 1:
+                    str_val = "Ace"
+                elif i == 11:
+                    str_val = "Jack"
+                elif i == 12:
+                    str_val = "Queen"
+                elif i == 13:
+                    str_val = "King"
+                else:
+                    str_val = str(i)
+                self.cards.append( card.Card( s , i , str_val ) )
+
+    def show_cards(self):
+        for card in self.cards:
+            card.card_info()
+
+    def empty_deck(self):
+        self.cards.clear()
+
+    def randomize(self):
+        new_deck = Deck()
+        new_deck.empty_deck()
+        while len(self.cards):
+            random_number = random.randrange(0,len(self.cards))
+            new_deck.cards.append(self.cards[random_number])
+            self.cards.pop(random_number)
+        self.cards = new_deck.cards
+
+
---
+++ game.py
@@ -0,0 +1,10 @@
+from classes.deck import Deck
+
+bicycle = Deck()
+
+bicycle.show_cards()
+
+print("********************************************")
+bicycle.randomize()
+
+bicycle.show_cards()
